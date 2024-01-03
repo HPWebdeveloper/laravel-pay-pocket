413e1a1e5fb7f6527b239442f3c79f4c8001dee5
3m1n3nc3 <mygames.ng@gmail.com>
2024-01-03 06:18:56 +0100

    Change $restrictedWallets param to $allowedWallets

--- src/Traits/HandlesPayment.php
+++ src/Traits/HandlesPayment.php
@@ -13,19 +13,25 @@ trait HandlesPayment
      *
      * @throws InsufficientBalanceException
      */
-    public function pay(int|float $orderValue, array $restrictedWallets = [], ?string $detail = null): void
+    public function pay(int|float $orderValue, array $allowedWallets = [], ?string $detail = null): void
     {
         if (! $this->hasSufficientBalance($orderValue)) {
             throw new InsufficientBalanceException('Insufficient balance to cover the order.');
         }
 
-        DB::transaction(function () use ($orderValue, $detail, $restrictedWallets) {
+        DB::transaction(function () use ($orderValue, $detail, $allowedWallets) {
             $remainingOrderValue = $orderValue;
 
             $walletsInOrder = $this->wallets()->whereIn('type', $this->walletsInOrder())->get();
 
+            /**
+             * @param string $wallet
+             * @return bool $useWallet
+             * */
+            $useWallet = fn ($wallet) => count($allowedWallets) < 1 || in_array($wallet, $allowedWallets);
+
             foreach ($walletsInOrder as $wallet) {
-                if (! $wallet || ! $wallet->hasBalance() || in_array($wallet, $restrictedWallets)) {
+                if (! $wallet || ! $wallet->hasBalance() || !$useWallet($wallet->type->value)) {
                     continue;
                 }
 
