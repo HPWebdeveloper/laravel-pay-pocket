260da62c1e71959ade6f5108d0f7f87c73f3cca5
3m1n3nc3 <mygames.ng@gmail.com>
2024-01-03 10:35:14 +0100

    Fix Facade annotations

--- src/Facades/LaravelPayPocket.php
+++ src/Facades/LaravelPayPocket.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Facade;
 /**
  * @see \HPWebdeveloper\LaravelPayPocket\Services\PocketServices
  *
- * @method void pay($user, $orderValue, array $allowedWallets = [], ?string $detail = null)
+ * @method void pay($user, int|float $orderValue, array $allowedWallets = [], ?string $detail = null)
  * @method bool deposit($user, string $type, int|float $amount, ?string $detail = null)
  * @method int|float checkBalance($user)
  * @method int|float walletBalanceByType($user, string $type)
