55e72f0a3ba7364c3e83448ab58c2f0976f12280
Sajjad Esmaeeli <s.sajjad.esmaeeli@gmail.com>
2024-04-12 22:19:28 +0200

    bring back comment

--- config/pay-pocket.php
+++ config/pay-pocket.php
@@ -1,6 +1,27 @@
 <?php
 
+// config for HPWebdeveloper/LaravelPayPocket
+
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Reference Generator Configuration
+    |--------------------------------------------------------------------------
+    |
+    | This configuration allows you to customize the generation of log reference strings
+    | within the LaravelPayPocket package.
+    |
+    | - log_reference_length: The length of the generated string.
+    | - log_reference_prefix: The prefix for the generated string.
+    | - log_reference_generator_class: The fully qualified name of the class containing static methods for generation.
+    | - log_reference_generator_method: The name of the static method available in the generator class.
+    |
+    | This is how it works in the code:
+    | Illuminate\Support\Str::random(12)
+    |
+    */
+
     'log_reference_length' => 12,
     'log_reference_prefix' => '',
     'log_reference_generator_class' => Illuminate\Support\Str::class,
