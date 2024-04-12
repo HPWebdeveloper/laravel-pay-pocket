c56b512f4a10e1f51ea76eefe94a3fc4837e59d1
SSEsmaeeli <SSEsmaeeli@users.noreply.github.com>
2024-04-12 20:00:58 +0000

    Fix styling

--- config/pay-pocket.php
+++ config/pay-pocket.php
@@ -5,10 +5,10 @@
 /**
  * Configuration for generating log reference strings.
  *
- * @param int    $log_reference_length           The length of the generated string.
- * @param string $log_reference_prefix            The prefix for the generated string.
- * @param string $log_reference_generator_class  The fully qualified name of the class containing static methods for generation.
- * @param string $log_reference_generator_method The name of the static method available in the generator class.
+ * @param  int  $log_reference_length  The length of the generated string.
+ * @param  string  $log_reference_prefix  The prefix for the generated string.
+ * @param  string  $log_reference_generator_class  The fully qualified name of the class containing static methods for generation.
+ * @param  string  $log_reference_generator_method  The name of the static method available in the generator class.
  *
  * This is how it works in the code:
  * Illuminate\Support\Str::random(12)
