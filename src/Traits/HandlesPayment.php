43d14bcb9cbe957045c1bc0878ad608b0a8a3e8b
3m1n3nc3 <mygames.ng@gmail.com>
2024-06-29 21:22:23 +0100

    Run pint

--- src/Traits/HandlesPayment.php
+++ src/Traits/HandlesPayment.php
@@ -11,15 +11,14 @@ trait HandlesPayment
     /**
      * Pay the order value from the user's wallets.
      *
-     * @param int|float $orderValue
-     * @param ?string $notes
+     * @param  ?string  $notes
+     * @return \Illuminate\Support\Collection<TKey,WalletsLog>
      *
      * @throws InsufficientBalanceException
-     * @return \Illuminate\Support\Collection<TKey,WalletsLog>
      */
     public function pay(int|float $orderValue, ?string $notes = null): \Illuminate\Database\Eloquent\Collection
     {
-        if (!$this->hasSufficientBalance($orderValue)) {
+        if (! $this->hasSufficientBalance($orderValue)) {
             throw new InsufficientBalanceException('Insufficient balance to cover the order.');
         }
 
@@ -34,7 +33,7 @@ trait HandlesPayment
             $logs = (new WalletsLog())->newCollection();
 
             foreach ($walletsInOrder as $wallet) {
-                if (!$wallet || !$wallet->hasBalance()) {
+                if (! $wallet || ! $wallet->hasBalance()) {
                     continue;
                 }
 
